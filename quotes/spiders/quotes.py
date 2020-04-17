7e097d3a5016f0c033f20714995f77a8a022a245
Pascal Reinhardt <kalle.reinhardt@gmail.com>
2020-04-17 19:37:13 +0200

    change spider from http://quotes.toscrape.com/random http://quotes.toscrape.com

--- quotes/spiders/quotes.py
+++ quotes/spiders/quotes.py
@@ -5,11 +5,13 @@ import scrapy
 class quotesSpider(scrapy.Spider):
     name = 'quotes'
     allowed_domains = ['toscrape.com']
-    start_urls = ['http://quotes.toscrape.com/random']
+    start_urls = ['http://quotes.toscrape.com']
 
     def parse(self, response):
-        yield{
-        	'author_name': response.css('small.author::text').extract_first(),
-        	'text': response.css('span.text::text').extract_first(),
-        	'tags': response.css('a.tag::text').extract()
-        }
+    	for quote in response.css('div.quote')
+	        item = {
+	        	'author_name': response.css('small.author::text').extract_first(),
+	        	'text': response.css('span.text::text').extract_first(),
+	        	'tags': response.css('a.tag::text').extract()
+	        }
+	        yield item
