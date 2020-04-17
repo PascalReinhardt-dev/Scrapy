2e95bcdb4dced294bac5b73cb1820212b9588ba3
Pascal Reinhardt <kalle.reinhardt@gmail.com>
2020-04-17 19:40:45 +0200

    add missed ;

--- quotes/spiders/quotes.py
+++ quotes/spiders/quotes.py
@@ -8,7 +8,7 @@ class quotesSpider(scrapy.Spider):
     start_urls = ['http://quotes.toscrape.com']
 
     def parse(self, response):
-    	for quote in response.css('div.quote')
+    	for quote in response.css('div.quote'):
 	        item = {
 	        	'author_name': response.css('small.author::text').extract_first(),
 	        	'text': response.css('span.text::text').extract_first(),
